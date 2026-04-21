eae1df4081bc6485ab0c354200835685f7c258c1
Youssef_Abdo <96637360+yousef21321@users.noreply.github.com>
2026-04-21 20:41:28 +0200

    make TaskFactory for fake data

--- database/factories/TaskFactory.php
+++ database/factories/TaskFactory.php
@@ -1,23 +1,17 @@
 <?php
-
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Task>
- */
 class TaskFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
     public function definition(): array
     {
         return [
-            //
+            'title' => fake()->sentence(4),
+            'description' => fake()->paragraph(),
+            'status' => fake()->randomElement(['pending', 'in_progress', 'done']),
+            'due_date' => fake()->dateTimeBetween('now', '+1 month')->format('Y-m-d'),
         ];
     }
 }
