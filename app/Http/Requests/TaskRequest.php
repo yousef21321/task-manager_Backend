5f92806dbeae9efd7b52b451ae422a01bca51a32
Youssef_Abdo <96637360+yousef21321@users.noreply.github.com>
2026-04-21 20:13:32 +0200

    make Form Request Validation data for Tasks

--- app/Http/Requests/TaskRequest.php
+++ app/Http/Requests/TaskRequest.php
@@ -6,23 +6,19 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class TaskRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
+
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
-     */
     public function rules(): array
     {
         return [
-            //
+            'title' => 'required|string|max:255',
+            'description' => 'nullable|string',
+            'status' => 'nullable|in:pending,in_progress,done',
+            'due_date' => 'required|date',
         ];
     }
 }
